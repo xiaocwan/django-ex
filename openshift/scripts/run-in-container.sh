99f235dd84bac4884b02189ee588b124b4935a48
Xiaochuan Wang <xiaocwan@redhat.com>
2018-12-18 16:45:56 +0800

    Update run-in-container.sh

--- openshift/scripts/run-in-container.sh
+++ openshift/scripts/run-in-container.sh
@@ -32,10 +32,11 @@
 #     POD_NAME=frontend POD_INDEX=2 ./run-in-container.sh ./manage.py shell
 
 
-# Get name of a currently deployed pod by label and index
+## Get name of a currently deployed pod by label and index
 POD_INSTANCE_NAME=`oc get pods \
   -l "name=${POD_NAME:-django-frontend}" \
   -t "{{ with index .items ${POD_INDEX:-0} }}{{ .metadata.name }}{{ end }}"`
 
 # Run command in a container of the specified pod:
-oc exec -p "$POD_INSTANCE_NAME" -it -- bash -c "${@:-echo}"
+#oc exec -p "$POD_INSTANCE_NAME" -it -- bash -c "${@:-echo}"
+oc exec -p "$POD_INSTANCE_NAME" -it -- bash -c "scrapy runspider ../../../quotes_spider.py -o ../../../result.json"
